5482177d69f58cd6e6f90a0addd3d56cb4772455
Yudhi Hartawan <Yudhi.Hartawan@hills.com.au>
2018-10-31 11:33:28 +1100

    Add POST/todos routes and refactor mongoose

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -21,9 +21,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     //     console.log(result);
     // })
 
-    db.collection('Todos').
-
-    
     //client.close();
 }
 );
