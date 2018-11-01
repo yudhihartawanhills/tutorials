771b537d70ea9c8d621e564237b1356d18a13ee7
Yudhi Hartawan <Yudhi.Hartawan@hills.com.au>
2018-11-01 13:51:33 +1100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} =require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 
 var app = express();
@@ -11,21 +11,31 @@ var app = express();
 app.use(bodyParser.json());
 
 //create new todos
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text:req.body.text
+        text: req.body.text
     })
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(err)=>{
+    }, (err) => {
         res.status(400).send(err);
     })
-    
+
 })
 
-app.listen(3000,()=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    }).catch((err) => { console.log(err); })
+    });
+
+app.listen(3000, () => {
     console.log("Started on port 3000");
 })
 
 
-module.exports={app};
+
+
+module.exports = { app };
