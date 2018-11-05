e3ed8e063003e142d0462d299beb2e6795569042
yudhihartawanhills <yudhi.hartawan@hills.com.au>
2018-11-06 09:07:23 +1100

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var { ObjectId} = require('mongodb');
+var { ObjectId } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
